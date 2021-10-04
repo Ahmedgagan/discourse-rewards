3b91e0cee1ce95a442dfca2255930f32be5cd00d
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-10-04 20:34:04 +0530

    DEV: removes redemeed points table

--- app/models/redemeed_point.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module DiscourseRewards
-  class RedemeedPoint < ActiveRecord::Base
-    self.table_name = 'discourse_rewards_redemeed_points'
-
-    belongs_to :user
-  end
-end
